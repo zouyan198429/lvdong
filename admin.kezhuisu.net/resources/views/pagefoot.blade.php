c3b7300fbdbe545bd0dd3c09b6bcb1b63bda6cf0
zouyan198429 <305463219@qq.com>
2018-07-14 08:18:04 +0800

    更新

--- admin.kezhuisu.net/resources/views/pagefoot.blade.php
+++ admin.kezhuisu.net/resources/views/pagefoot.blade.php
@@ -1,6 +1,6 @@
 @include('public.dynamic_list_foot')
+<script src="{{asset('dist/js/zui.min.js')}}"></script>
 <script src="{{asset('assets/js/jquery.min.js')}}"></script>
 <script src="{{asset('assets/js/amazeui.min.js')}}"></script>
 <script src="{{asset('assets/js/iscroll.js')}}"></script>
 <script src="{{asset('assets/js/app.js')}}"></script>
-<script src="{{asset('dist/js/zui.min.js')}}"></script>

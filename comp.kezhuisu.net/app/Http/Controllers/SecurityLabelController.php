6618fa95e6649dd3b562f9e2e6ba80dd44a37f0e
zouyan198429 <305463219@qq.com>
2018-09-02 21:28:22 +0800

    列表权限的问题

--- comp.kezhuisu.net/app/Http/Controllers/SecurityLabelController.php
+++ comp.kezhuisu.net/app/Http/Controllers/SecurityLabelController.php
@@ -121,6 +121,15 @@ class SecurityLabelController extends LoginController
     public function ajax_alist(Request $request,$pro_unit_id){
         $this->InitParams($request);
 
+        $relations = '';
+        $resultDatas = $this->getinfoApi('CompanyProUnit', $relations, $this->company_id , $pro_unit_id);
+        // 判断权限
+        $judgeData = [
+            'company_id' => $this->company_id,
+            'id' => $pro_unit_id,
+        ];
+        $this->judgePowerByObj($request,$resultDatas, $judgeData );
+
         // 获得翻页的三个关键参数
         $pageParams = Common::getPageParams($request);
         list($page, $pagesize, $total) = array_values($pageParams);
